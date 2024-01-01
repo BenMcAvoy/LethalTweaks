bb1e5e4f2b68afcfea23b8144f3f4b8c17b5f3bb
BenMcAvoy <ben.mcavoy@tutanota.com>
2024-01-01 15:40:36 +0000

    Rename to `LethalTweaks`

---
+++ LethalTweaks/obj/Debug/netstandard2.1/PluginInfo.cs
@@ -0,0 +1,9 @@
+namespace LethalTweaks
+{
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "LethalTweaks";
+        public const string PLUGIN_NAME = "LethalTweaks";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
