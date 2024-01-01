6c97611b51834225248f3169da546fdc8b2241f2
BenMcAvoy <ben.mcavoy@tutanota.com>
2024-01-01 19:54:57 +0000

    Update to LethalTweaks and add refernces

--- LethalTweaks/Plugin.cs
+++ LethalTweaks/Plugin.cs
@@ -1,14 +1,19 @@
-﻿using BepInEx;
+﻿using LethalCompanyInputUtils.Api;
+using UnityEngine.InputSystem;
+using BepInEx;
 
-namespace LethalTweaks
-{
+namespace LethalTweaks {
+    public class LethalTweaksInput : LcInputActions {
+        [InputAction("<Keyboard>/1", Name = "Slot 1")]
+        public InputAction SlotOne { get; set; }
+    }
+
     [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
-    public class Plugin : BaseUnityPlugin
-    {
-        private void Awake()
-        {
-            // Plugin startup logic
+    public class Plugin : BaseUnityPlugin {
+        private void Awake() {
             Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+
+            
         }
     }
 }
--- LethalTweaks/obj/Release/netstandard2.1/PluginInfo.cs
+++ LethalTweaks/obj/Release/netstandard2.1/PluginInfo.cs
@@ -1,9 +1,9 @@
-namespace LethalCompanyTemplate
+namespace LethalTweaks
 {
     public static class PluginInfo
     {
-        public const string PLUGIN_GUID = "LethalCompanyTemplate";
-        public const string PLUGIN_NAME = "LethalCompanyTemplate";
+        public const string PLUGIN_GUID = "LethalTweaks";
+        public const string PLUGIN_NAME = "LethalTweaks";
         public const string PLUGIN_VERSION = "1.0.0";
     }
 }
