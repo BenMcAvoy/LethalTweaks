c1bc7cc256836229734149bd21d092eb8e04ce25
Ben McAvoy <ben.mcavoy@tutanota.com>
2024-01-03 18:44:21 +0000

    Reformat and remove unnecessary `using` statements

--- LethalTweaks/Plugin.cs
+++ LethalTweaks/Plugin.cs
@@ -1,43 +1,30 @@
-﻿using BepInEx;
-using BepInEx.Logging;
-using HarmonyLib;
-using LethalTweaks.Patches;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using BepInEx.Logging;
+using BepInEx;
 
-namespace LethalTweaks
-{
+using LethalTweaks.Patches;
+using HarmonyLib;
+
+namespace LethalTweaks {
     [BepInPlugin(modGUID, modName, modVerison)]
-    public class LethalTweaksBase : BaseUnityPlugin
-    {
+    public class LethalTweaksBase : BaseUnityPlugin {
         public const string modGUID = "HydrxAndBen.LethalTweaks";
         public const string modName = "Lethal Tweaks";
         public const string modVerison = "0.0.1";
 
         private readonly Harmony harmony = new Harmony(modGUID);
-
         private static LethalTweaksBase Instance;
+        internal ManualLogSource logger;
 
-        internal ManualLogSource mls;
-
-
-        void Awake()
-        {
+        void Awake() {
             if (Instance == null)
-            {
                 Instance = this;
-            }
 
-            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+            logger = BepInEx.Logging.Logger.CreateLogSource(modGUID);
 
-            mls.LogInfo("Lethal Tweaks has started");
+            logger.LogInfo("Lethal Tweaks has started");
 
             harmony.PatchAll(typeof(LethalTweaksBase));
             harmony.PatchAll(typeof(PlayerControllerBPatch));
         }
-
     }
 }
