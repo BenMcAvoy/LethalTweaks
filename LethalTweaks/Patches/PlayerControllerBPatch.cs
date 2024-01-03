6bc403c1eb855df196b78b00e732da3d814178ef
Ben McAvoy <ben.mcavoy@tutanota.com>
2024-01-03 18:54:34 +0000

    Reformat and remove unnecessary `using` statements

--- LethalTweaks/Patches/PlayerControllerBPatch.cs
+++ LethalTweaks/Patches/PlayerControllerBPatch.cs
@@ -1,20 +1,12 @@
 ﻿using GameNetcodeStuff;
 using HarmonyLib;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace LethalTweaks.Patches
-{
+namespace LethalTweaks.Patches {
     [HarmonyPatch(typeof(PlayerControllerB))]
-    internal class PlayerControllerBPatch
-    {
+    internal class PlayerControllerBPatch {
         [HarmonyPatch("Update")]
         [HarmonyPostfix]
-        static void infiniteSprintPatch(ref float ___sprintMeter)
-        {
+        static void infiniteSprintPatch(ref float ___sprintMeter) {
             ___sprintMeter = 1f;
         }
     }
