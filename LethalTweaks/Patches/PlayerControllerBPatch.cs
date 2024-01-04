1c2f0467920f72995fe813a0983b4cd7a54f91a1
Ben McAvoy <ben.mcavoy@tutanota.com>
2024-01-04 03:15:15 +0000

    Implement flashligh toggling with keyboard

--- LethalTweaks/Patches/PlayerControllerBPatch.cs
+++ LethalTweaks/Patches/PlayerControllerBPatch.cs
@@ -1,4 +1,7 @@
-﻿using GameNetcodeStuff;
+﻿using LethalCompanyInputUtils.Api;
+using UnityEngine.InputSystem;
+using GameNetcodeStuff;
+using UnityEngine;
 using HarmonyLib;
 
 using System.Collections.Generic;
@@ -6,14 +9,53 @@ using System.Reflection.Emit;
 using System.Linq;
 using System;
 
+public class Keybinds : LcInputActions {
+    [InputAction("<Keyboard>/f", Name = "Flashlight")]
+    public InputAction Flashlight { get; set; }
+}
+
 namespace LethalTweaks.Patches {
     [HarmonyPatch(typeof(PlayerControllerB))]
     internal class PlayerControllerBPatch {
-        /*[HarmonyPatch("Update")]
+        internal static Keybinds InputActionInstance = new Keybinds();
+
+        [HarmonyPatch("KillPlayer")]
         [HarmonyPostfix]
-        static void infiniteSprintPatch(ref float ___sprintMeter) {
-            ___sprintMeter = 1f;
-        }*/
+        public static void ClearFlashlight(PlayerControllerB __instance) {
+            __instance.pocketedFlashlight = null;
+        }
+
+        [HarmonyPatch("Update")]
+        [HarmonyPostfix]
+        public static void Update(PlayerControllerB __instance, ref float ___sprintMeter)
+        {
+            if (!ShouldProcess(__instance)) return;
+
+            if (__instance.currentlyHeldObjectServer is FlashlightItem flashlightItem && __instance.currentlyHeldObjectServer != __instance.pocketedFlashlight)
+                __instance.pocketedFlashlight = __instance.currentlyHeldObjectServer;
+
+            if (__instance.pocketedFlashlight != null && InputActionInstance.Flashlight.triggered && __instance.pocketedFlashlight is FlashlightItem pocketedFlashlight && pocketedFlashlight.isHeld) {
+                try {
+                    pocketedFlashlight.UseItemOnClient(true);
+
+                    if (!(__instance.currentlyHeldObjectServer is FlashlightItem))
+                        HandleFlashlightUsage(__instance, pocketedFlashlight);
+                } catch { }
+            }
+        }
+
+        private static bool ShouldProcess(PlayerControllerB __instance) {
+            return !(!__instance.IsOwner || !__instance.isPlayerControlled || (__instance.IsServer && !__instance.isHostPlayerObject)) && !__instance.isTestingPlayer && !__instance.inTerminalMenu && !__instance.isTypingChat && Application.isFocused;
+        }
+
+        private static void HandleFlashlightUsage(PlayerControllerB __instance, FlashlightItem pocketedFlashlight) {
+            pocketedFlashlight.flashlightBulbGlow.enabled = false;
+            pocketedFlashlight.flashlightBulb.enabled = false;
+
+            __instance.helmetLight.enabled = pocketedFlashlight.isBeingUsed;
+            pocketedFlashlight.usingPlayerHelmetLight = pocketedFlashlight.isBeingUsed;
+            pocketedFlashlight.PocketFlashlightServerRpc(pocketedFlashlight.isBeingUsed);
+        }
 
         [HarmonyPatch("LateUpdate")]
         [HarmonyPrefix]
