be0fb2bcb3006b46a9c503c888c27c9c587549fd
Ben McAvoy <ben.mcavoy@tutanota.com>
2024-01-04 03:49:46 +0000

    Begin enabled dev mode

---
+++ LethalTweaks/Patches/devPatches.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using System;
+using UnityEngine;
+
+namespace LethalTweaks.Patches {
+    internal class devPatches {
+        const bool enabled = false;
+
+        [HarmonyPatch(typeof(Application), "isEditor", MethodType.Getter)]
+        static bool IsEditorGetter(ref bool __result) {
+            Console.WriteLine("Being called");
+            __result = enabled;
+            return false;
+        }
+
+        [HarmonyPatch(typeof(QuickMenuManager), "OpenQuickMenu")]
+        [HarmonyPostfix]
+        private static void openQuickMenu(QuickMenuManager __instance) {
+            Console.WriteLine("Opening quick menu");
+            Console.WriteLine(__instance.debugMenuUI.activeSelf);
+            __instance.debugMenuUI.SetActive(true);
+        }
+    }
+}
