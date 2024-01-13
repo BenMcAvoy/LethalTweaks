8d3178fffd3cc68d546cdaae6749630a15d170b7
Ben McAvoy <ben.mcavoy@tutanota.com>
2024-01-13 21:47:22 +0000

    Add FlashLightPatch

---
+++ LethalTweaks/Patches/FlashlightPatch.cs
@@ -0,0 +1,19 @@
+﻿using GameNetcodeStuff;
+using HarmonyLib;
+using System.Reflection;
+
+namespace LethalTweaks.Patches {
+    internal class FlashlightPatch {
+        private static FieldInfo _previousPlayerField;
+
+        [HarmonyPatch(typeof(FlashlightItem), "DiscardItem")]
+        [HarmonyPostfix]
+        private static void DiscardItem(FlashlightItem __instance) {
+            if (_previousPlayerField == null)
+                _previousPlayerField = typeof(FlashlightItem).GetField("previousPlayerHeldBy", BindingFlags.Instance | BindingFlags.NonPublic);
+
+            var field = _previousPlayerField.GetValue(__instance);
+            PlayerControllerB controller = (PlayerControllerB)((field is PlayerControllerB) ? field : null);
+        }
+    }
+}
